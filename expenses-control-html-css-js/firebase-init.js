2aac8db1b6bbe58ae88512bd5323d2f6e1f457f0
Christyan95 <chrisaraujo124@gmail.com>
2024-09-18 09:43:27 -0300

    Front-end

---
+++ expenses-control-html-css-js/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDl9TQnonCi7mBe8wBEEABuWE6bpu9R3ME',
+  authDomain: 'controle-de-gastos-d1294.firebaseapp.com',
+  projectId: 'controle-de-gastos-d1294',
+  storageBucket: 'controle-de-gastos-d1294.appspot.com',
+  messagingSenderId: '401130534102',
+  appId: '1:401130534102:web:6e0fe93b08ca917c928577',
+};
+firebase.initializeApp(firebaseConfig);
